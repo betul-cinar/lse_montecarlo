3db2e8f76933b63d4a5181b2b57f5e7392fcf3bc
betul-cinar <betulcinar1104@gmail.com>
2025-05-21 20:35:23 +0300

    new scripts
    for jwang

--- getting_overall_error.m
+++ getting_overall_error.m
@@ -9,15 +9,32 @@ overallMean_20 = mean(error_map_20.ERROR_MAP, 'all');
 error_map_12 = load("ERRROR_MAP_n12_MAE.mat");
 overallMean_12 = mean(error_map_12.ERROR_MAP, 'all');
 
+error_map_jspringer = load("ERROR_MAP_JSPRINGER.mat");
+overallMean_jspringer = mean(error_map_jspringer.ERROR_MAP, 'all');
+
+error_map_jwang = load("ERROR_MAP_JWANG.mat");
+overallMean_jwang = mean(error_map_jwang.ERROR_MAP, 'all');
+
 % Calculate percentage of values <= 1 for error_map_20
 numBelowOne_20 = sum(error_map_20.ERROR_MAP(:) <= 1);
 totalPoints_20 = numel(error_map_20.ERROR_MAP);
 percentageBelowOne_20 = (numBelowOne_20 / totalPoints_20) * 100;
 
+
 % Calculate percentage of values <= 1 for error_map_12
-numBelowOne_12 = sum(error_map_12.ERROR_MAP(:) <= 2);
+numBelowOne_12 = sum(error_map_12.ERROR_MAP(:) <= 1);
 totalPoints_12 = numel(error_map_12.ERROR_MAP);
 percentageBelowOne_12 = (numBelowOne_12 / totalPoints_12) * 100;
+
+% Calculate percentage of values <= 1 for error_map_20
+numBelowOne_jspringer = sum(error_map_jspringer.ERROR_MAP(:) <= 1);
+totalPoints_jspringer = numel(error_map_jspringer.ERROR_MAP);
+percentageBelowOne_jspringer = (numBelowOne_jspringer / totalPoints_jspringer) * 100;
+
+% Calculate percentage of values <= 1 for error_map_20
+numBelowOne_jwang = sum(error_map_jwang.ERROR_MAP(:) <= 1);
+totalPoints_jwang = numel(error_map_jwang.ERROR_MAP);
+percentageBelowOne_jwang = (numBelowOne_jwang / totalPoints_jwang) * 100;
 
 % Display the results
 fprintf('n = 20 → Mean Error: %.4f | Points ≤ 1: %d (%.2f%%)\n', ...
@@ -25,3 +42,9 @@ fprintf('n = 20 → Mean Error: %.4f | Points ≤ 1: %d (%.2f%%)\n', ...
 
 fprintf('n = 12 → Mean Error: %.4f | Points ≤ 1: %d (%.2f%%)\n', ...
     overallMean_12, numBelowOne_12, percentageBelowOne_12);
+
+fprintf('n = jspringer → Mean Error: %.4f | Points ≤ 1: %d (%.2f%%)\n', ...
+    overallMean_jspringer, numBelowOne_jspringer, percentageBelowOne_jspringer);
+
+fprintf('n = jwang → Mean Error: %.4f | Points ≤ 1: %d (%.2f%%)\n', ...
+    overallMean_jwang, numBelowOne_jwang, percentageBelowOne_jwang);
---
+++ lse_jwang.m
@@ -0,0 +1,145 @@
+close all;
+clc;
+clear;
+
+%%%%%%%%%%%%%% USER INPUT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Define cones with different half-angles
+n = 16; %HOW MANY CONES?
+
+
+%WE START TO SIMULATE SUN-SUNSENSOR INTERACTION HERE
+
+coeff = load("coeff_sensor_sfh.mat");
+coeff_inv = load("coeff_inverse_sfh.mat");
+
+%USER INPUT
+sensor_az_body = [ ...
+     0,  22.5,  45.0,  67.5,  90.0, 112.5, 135.0, 157.5, ...
+   180.0, 202.5, 225.0, 247.5, 270.0, 292.5, 315.0, 337.5 ];
+
+sensor_el_body = [ ...
+    63.6, 63.6, 63.6, 63.6, 63.6, 63.6, 63.6, 63.6, ...
+    63.6, 63.6, 63.6, 63.6, 63.6, 63.6, 63.6, 63.6 ];
+
+%SUN COMING 
+sun_el = 70;
+sun_az = 140;
+
+
+% Define cone directions (unit vectors)
+az_el = [sensor_az_body' sensor_el_body' ];
+
+
+
+for i=1:n
+    
+    sens_val(i) = compute_photodiode(sensor_el_body(i), sensor_az_body(i), sun_el, sun_az, coeff);
+    
+    % halfangle_deg(i) = (1 - sens_val(i)) * 90;
+    halfangle_deg(i) = photodiode_model_inverse(sens_val(i), coeff_inv);
+end
+
+
+ halfangle = halfangle_deg .* (pi/180);      
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ekleme
+
+a =0;
+for i=1:n
+
+    if halfangle_deg(i) < 60 && halfangle_deg(i) > 0
+        a = a+1;
+        sel_numbers(a) = i; % to keep the track of selected sensor numbers
+        sel_azel(a,:) = az_el(i,:);
+        
+    end
+end
+
+% if a == 0
+%     return 
+% end
+
+
+   
+
+for i=1:a
+    % for j=1:3
+    nv_sel(i,:) = sph2cart_unit(az_el(sel_numbers(i),:));
+    halfangle_sel(i) = halfangle(sel_numbers(i));
+end
+% 
+% for i=1:a 
+% 
+%         nv_sel(i,:) =normalVec(i,:)/norm(normalVec(i,:));
+% 
+% end    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% Create a figure for plotting
+figure('Name','Three Cones Intersection Approximation');
+hold on; axis equal; grid on;                             
+xlabel('X'); ylabel('Y'); zlabel('Z');
+title('Selected Sensors FOVs and Their Approximate Intersection');
+view(3);
+
+
+colors = ['b','g','m','c','y','r'];
+for j = 1:a
+    colorChoice = colors(mod(j-1, length(colors))+1);
+    plotCone(halfangle_sel(j), nv_sel(j, :), colorChoice,true);
+
+    if j ==3
+        plotCone(halfangle_sel(j), nv_sel(j, :), colorChoice,false);
+
+    end
+end
+
+%Least Squares Optimization
+%Initial guess (average of the three normal vectors) ??? CHECK !
+upper = 0;
+for i= 1:a
+    upper = upper + nv_sel(i,:);
+end
+u0 = upper/ norm(upper);
+% u0 = (nv_sel(1,:) + nv_sel(2,:) + nv_sel(3,:)) / norm(nv_sel(1,:) + nv_sel(2,:) + nv_sel(3,:));
+
+% Define anonymous function for residuals
+residualFunc = @(u) coneResiduals(a, u, nv_sel, halfangle_sel);      
+
+
+% Solve using lsqnonlin
+options = optimoptions('lsqnonlin', 'Display', 'iter');
+u_solution = lsqnonlin(residualFunc, u0, [], [], options);
+
+% Display the solution
+disp('Approximate intersection vector:');
+disp(u_solution);
+
+display(  rad2deg(  atan2( u_solution(3), sqrt(  sum(u_solution(1:2).^2 )  )  )  )  );
+display( rad2deg(atan2(u_solution(2), u_solution(1))));
+% Plot the approximate intersection
+% quiver3(0, 0, 0, u_solution(1), u_solution(2), u_solution(3), 1, 'r', 'LineWidth', 3);
+
+% Plot the approximate intersection
+h_intersection = quiver3(0, 0, 0, u_solution(1), u_solution(2), u_solution(3), 1, 'r', 'LineWidth', 3, 'DisplayName','LS Estimated Sun Vector');
+
+% Add legend
+legend
+
+
+
+
+
+
